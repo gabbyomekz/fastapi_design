1e059648cec198127bd631df1051efb0f3caec98
Gabriel Omeke <gabbyomekz92@gmail.com>
2024-10-10 15:50:17 +0100

    query params string validation in fastapi

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 from pydantic import BaseModel
 
 app = FastAPI()
 
-# request body in fastapi
+# query params string validation in fastapi
 
-class Utensil(BaseModel):
-    name: str
-    color: str
-    make: str
-    description: str | None = None
-    cost: float
-    tax: float | None = None
-
-
-@app.post("/utensils")
-async def create_items(utensil: Utensil):
-    utensil_dict = utensil.model_dump()
-    if utensil.tax:
-        cost_with_tax = utensil.cost + utensil.tax
-        utensil_dict.update({"cost_with_tax": cost_with_tax})
-    return utensil_dict
-
-
-@app.put("/utensils/{utensil_id}")
-async def create_utensil_with_put(utensil_id: int, utensil: Utensil, q: str | None = None):
-    result = {"utensil_id": utensil_id, **utensil.model_dump()}
+@app.get("utensils")
+async def read_utensils(
+    q: str
+    | None = Query(
+        None,
+        min_length=3,
+        max_length=15,
+        title="kitchen utensils",
+        description="This is a sample query about kitchen utensils.",
+        alias="utensil-query"
+    )
+):
+    results = {"utensils": [{"utensil_id": "plates"}, {"utensil_id": "spoon"}]}
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    return results
+
+
+@app.get("/utensils_hidden")
+async def hidden_query_route(
+    hidden_query: str | None = Query(None, include_in_schema=False)
+):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    return {"hidden_query": "Not found"}
