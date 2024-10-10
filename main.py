2ddc6323ae43526010253847ff624edf31b519e9
Gabriel Omeke <gabbyomekz92@gmail.com>
2024-10-10 15:31:47 +0100

    request body in fastapi

--- main.py
+++ main.py
@@ -1,45 +1,34 @@
 from enum import Enum
+
 from fastapi import FastAPI
+
+from pydantic import BaseModel
 
 app = FastAPI()
 
-# query parameters in fastapi
+# request body in fastapi
 
-cook_utensils_db = [{"utensil_name": "plates"}, {"utensil_name": "spoon"}, {"utensil_name": "fork"}]
+class Utensil(BaseModel):
+    name: str
+    color: str
+    make: str
+    description: str | None = None
+    cost: float
+    tax: float | None = None
 
 
-@app.get("/utensils")
-async def list_utensils(skip: int = 0, limit: int = 15):
-    return cook_utensils_db[skip: skip + limit]
+@app.post("/utensils")
+async def create_items(utensil: Utensil):
+    utensil_dict = utensil.model_dump()
+    if utensil.tax:
+        cost_with_tax = utensil.cost + utensil.tax
+        utensil_dict.update({"cost_with_tax": cost_with_tax})
+    return utensil_dict
 
 
-@app.get("/utensils/{utensil_id}")
-async def get_utensil(
-    utensil_id: str, sample_query_param: str, q: str | None = None, short: bool = False
-):
-    utensil = {"utensil_id": utensil_id, "sample_query_param": sample_query_param}
+@app.put("/utensils/{utensil_id}")
+async def create_utensil_with_put(utensil_id: int, utensil: Utensil, q: str | None = None):
+    result = {"utensil_id": utensil_id, **utensil.model_dump()}
     if q:
-        utensil.update({"q": q})
-    if not short:
-        utensil.update(
-            {
-                "description": "Utensils are very essential in the kitchen, can as well be dangerous"
-            }
-        )
-    return utensil
-
-
-@app.get("/users/{user_id}/utensils/{utensil_id}")
-async def get_user_utensil(
-    user_id: int, utensil_id: str, q: str | None = None, short: bool = False
-):
-    utensil = {"utensil_id": utensil_id, "owner_id": user_id}
-    if q:
-        utensil.update({"q": q})
-    if not short:
-        utensil.update(
-            {
-                "description": "Utensils are very essential in the kitchen, can as well be dangerous"
-            }
-        )
-    return utensil
+        result.update({"q": q})
+    return result
