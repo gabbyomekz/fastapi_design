4ba954f4fcdb720e30236e7e033c4cd71665dda2
Gabriel Omeke <gabbyomekz92@gmail.com>
2024-10-10 16:15:46 +0100

    path params numeric validation in fastapi

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
 from enum import Enum
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 
 from pydantic import BaseModel
 
 app = FastAPI()
 
-# request body in fastapi
+# path params numeric validation in fastapi
 
-class Utensil(BaseModel):
-    name: str
-    color: str
-    make: str
-    description: str | None = None
-    cost: float
-    tax: float | None = None
-
-
-@app.post("/utensils")
-async def create_utensil(utensil: Utensil):
-    utensil_dict = utensil.model_dump()
-    if utensil.tax:
-        cost_with_tax = utensil.cost + utensil.tax
-        utensil_dict.update({"cost_with_tax": cost_with_tax})
-    return utensil_dict
-
-
-@app.put("/utensils/{utensil_id}")
-async def create_utensil_with_put(utensil_id: int, utensil: Utensil, q: str | None = None):
-    result = {"utensil_id": utensil_id, **utensil.model_dump()}
+@app.get("/utensils_validation/{utensil_id}")
+async def read_utensils_validation(
+    *,
+    utensil_id: int = Path(..., title="The ID of the utensil to get", gt=10, le=120),
+    q: str = "hello",
+    size: float = Query(..., gt=0, lt=7.65)
+):
+    results = {"utensil_id": utensil_id, "size": size}
     if q:
-        result.update({"q": q})
-    return result
+        results.update({"q": q})
+    return results
