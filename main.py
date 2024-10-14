7983d20bb0a71def4f960dca8a12b363cc7caedd
Gabriel Omeke <gabbyomekz92@gmail.com>
2024-10-14 14:02:31 +0100

    cookie and header parameters in fastapi

--- main.py
+++ main.py
@@ -3,29 +3,25 @@ from datetime import datetime, time, timedelta
 from enum import Enum
 from uuid import UUID
 
-from fastapi import Body, FastAPI, Query, Path
+from fastapi import Body, FastAPI, Query, Path, Cookie, Header
 from pydantic import BaseModel, Field, HttpUrl
 
 app = FastAPI()
 
-# Extra Data Types in fastapi
+# Cookie and Header Parameters in fastapi
 
-@app.put("/sport_items/{sport_item_id}")
+@app.get("/sport_items")
 async def read_sport_items(
-    sport_item_id: UUID,
-    start_date: datetime | None = Body(None),
-    end_date: datetime | None = Body(None),
-    repeat_at: time | None = Body(None),
-    process_after: timedelta | None = Body(None)
+    cookie_id: str | None = Cookie(None),
+    accept_encoding: str | None = Header(None),
+    sec_ch_ua: str | None = Header(None),
+    user_agent: str | None = Header(None),
+    x_token: list[str] | None = Header(None),
 ):
-    start_process = start_date + process_after
-    duration = end_date - start_process
     return {
-        "sport_item_id": sport_item_id,
-        "start_date": start_date,
-        "end_date": end_date,
-        "repeat_at": repeat_at,
-        "process_after": process_after,
-        "start_process": start_process,
-        "duration": duration
+        "cookie_id": cookie_id,
+        "Accept-Encoding": accept_encoding,
+        "sec-ch-ua": sec_ch_ua,
+        "User-Agent": user_agent,
+        "X-Token values": x_token,
     }
