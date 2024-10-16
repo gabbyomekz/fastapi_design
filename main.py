51bdeb3e94b1fbdec9bb333561c219f4aab9d049
Gabriel Omeke <gabbyomekz92@gmail.com>
2024-10-16 17:32:17 +0100

    classes as dependencies in fastapi

--- main.py
+++ main.py
@@ -31,22 +31,21 @@ from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
-# dependencies intro in fastapi
+# classes as dependencies in fastapi
 
-async def hello():
-    return "fastapi world"
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-async def common_parameters(
-    q: str | None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
-):
-    return {"q": q, "skip": skip, "limit": limit, "hello": blah}
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
 
-@app.get("/items/")
-async def read_items(commons: dict = Depends(common_parameters)):
-    return commons
-
-@app.get("/users/")
-async def read_users(commons: dict = Depends(common_parameters)):
-    return commons
-
-
+@app.get("/items/{item_id}")
+async def read_items(commons: CommonQueryParams = Depends()):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip: commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
