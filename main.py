d19a7ad44f7d23fdd5b28fa96144e888543f3586
Gabriel Omeke <gabbyomekz92@gmail.com>
2024-10-16 17:19:46 +0100

    JSON compatible encoder in fastapi

--- main.py
+++ main.py
@@ -14,24 +14,66 @@ from fastapi import (
     Form,
     File,
     UploadFile,
+    HTTPException,
+    Request,
 )
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.responses import JSONResponse, PlainTextResponse
 from pydantic import BaseModel, Field, HttpUrl, EmailStr
+from starlette.exceptions import HTTPException as StarletteHTTPException
 from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
-# request forms and files in fastapi
+# JSON compatible encoder in fastapi
 
-@app.post("/files/")
-async def create_file(
-    file: bytes = File(...),
-    another_file: UploadFile = File(...),
-    token: str = Form(...),
-    hi: str = Body(...)
-):
-    return {
-        "file_size": len(file),
-        "token": token,
-        "another_file_content_type": another_file.content_type,
-        "hi": hi
+class Item(BaseModel):
+    name: str | None = None
+    description: str | None = None
+    price: float | None = None
+    tax: float = 12.8
+    tags: list[str] = []
+
+items = {
+    "foo": {"name": "Foo", "price": 50.2},
+    "bar": {
+        "name": "Bar",
+        "description": "These bar men are ready to serve your every need",
+        "price": 75,
+        "tax": 16.5,
+    },
+    "baz": {
+        "name": "Baz",
+        "description": None,
+        "price": 6.2,
+        "tax": 12.8,
+        "tags": [],
     }
+}
+
+@app.get("/items/{item_id}", response_model=Item)
+async def read_item(item_id: str):
+    return items.get(item_id)
+
+@app.put("/items/{item_id}", response_model=Item)
+def update_item(item_id: str, item: Item):
+    update_item_encoded = jsonable_encoder(item)
+    items[item_id] = update_item_encoded
+    return update_item_encoded
+
+@app.patch("/items/{item_id}", response_model=Item)
+def patch_item(item_id: str, item: Item):
+    stored_item_data = items.get(item_id)
+    if stored_item_data is not None:
+        stored_item_model = Item(**stored_item_data)
+    else:
+        stored_item_model = Item()
+    update_data = item.model_dump(exclude_unset=True)
+    updated_item = stored_item_model.copy(update=update_data)
+    items[item_id] = jsonable_encoder(updated_item)
+    return updated_item
