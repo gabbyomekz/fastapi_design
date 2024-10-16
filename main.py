9d39efb008c778c1283c03118ae5ef70d9e4e33a
Gabriel Omeke <gabbyomekz92@gmail.com>
2024-10-16 17:27:33 +0100

    dependencies intro in fastapi

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from uuid import UUID
 
 from fastapi import (
     Body,
+    Depends,
     FastAPI,
     Query,
     Path,
@@ -30,50 +31,22 @@ from starlette.responses import HTMLResponse
 
 app = FastAPI()
 
-# JSON compatible encoder in fastapi
+# dependencies intro in fastapi
 
-class Item(BaseModel):
-    name: str | None = None
-    description: str | None = None
-    price: float | None = None
-    tax: float = 12.8
-    tags: list[str] = []
+async def hello():
+    return "fastapi world"
 
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {
-        "name": "Bar",
-        "description": "These bar men are ready to serve your every need",
-        "price": 75,
-        "tax": 16.5,
-    },
-    "baz": {
-        "name": "Baz",
-        "description": None,
-        "price": 6.2,
-        "tax": 12.8,
-        "tags": [],
-    }
-}
+async def common_parameters(
+    q: str | None = None, skip: int = 0, limit: int = 100, blah: str = Depends(hello)
+):
+    return {"q": q, "skip": skip, "limit": limit, "hello": blah}
 
-@app.get("/items/{item_id}", response_model=Item)
-async def read_item(item_id: str):
-    return items.get(item_id)
+@app.get("/items/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
 
-@app.put("/items/{item_id}", response_model=Item)
-def update_item(item_id: str, item: Item):
-    update_item_encoded = jsonable_encoder(item)
-    items[item_id] = update_item_encoded
-    return update_item_encoded
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
 
-@app.patch("/items/{item_id}", response_model=Item)
-def patch_item(item_id: str, item: Item):
-    stored_item_data = items.get(item_id)
-    if stored_item_data is not None:
-        stored_item_model = Item(**stored_item_data)
-    else:
-        stored_item_model = Item()
-    update_data = item.model_dump(exclude_unset=True)
-    updated_item = stored_item_model.copy(update=update_data)
-    items[item_id] = jsonable_encoder(updated_item)
-    return updated_item
+
